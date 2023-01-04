665ab57d4292742d54fa5bc09537fdc2d93f5af4
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-01-04 03:44:53 -0600

    Testing linear layer

--- cudann/test/test_layers.cpp
+++ cudann/test/test_layers.cpp
@@ -1,21 +1,38 @@
 #include "../serial/cudann.h"
+#include <cstring>
 
 int main(int argc, char *argv[]) {
-    const int n_classes = 5;
-    const int n_features = 10;
+    const int out_features = 3;
+    const int in_features = 2;
     const int batch_size = 2;
-    std::string test_type = "backward";
+    std::string test_type = "forward";
 
     // Create a model
     NN model;
-    Softmax layer(n_classes, "layer1");
-    double *data = (double*) malloc(n_classes * batch_size * sizeof(double));
-    initialize_random(data, n_classes * batch_size);
+    Linear layer(in_features, out_features, true, "random", "layer1");
+    double *data = (double*) malloc(in_features * batch_size * sizeof(double));
+    initialize_random(data, in_features * batch_size);
     Tensor *input = (Tensor*) malloc(sizeof(Tensor));
-    initialize_tensor(input, batch_size, n_classes, data);
+    initialize_tensor(input, batch_size, in_features, data);
     if (test_type == "forward" || test_type == "backward") {
         input->print();
     }
+
+    double *weights = (double*) malloc(out_features * in_features * sizeof(double));
+    memcpy(weights, layer.weight, out_features * in_features * sizeof(double));
+    Tensor *weight_tensor = (Tensor*) malloc(sizeof(Tensor));
+    initialize_tensor(weight_tensor, out_features, in_features, weights);
+    double *bias = (double*) malloc(out_features * sizeof(double));
+    memcpy(bias, layer.bias, out_features * sizeof(double));
+    Tensor *bias_tensor = (Tensor*) malloc(sizeof(Tensor));
+    initialize_tensor(bias_tensor, 1, out_features, bias);
+    if (test_type == "forward" || test_type == "backward") {
+        weight_tensor->print();
+        bias_tensor->print();
+    }
+
+
+
 
     // Forward pass
     Tensor* output = layer.forward(input);
@@ -24,10 +41,10 @@ int main(int argc, char *argv[]) {
     }
 
     // Backward pass
-    double *grad = (double*) malloc(n_classes * batch_size * sizeof(double));
-    initialize_random(grad, n_classes * batch_size);
+    double *grad = (double*) malloc(out_features * batch_size * sizeof(double));
+    initialize_random(grad, out_features * batch_size);
     Tensor *grad_output = (Tensor*) malloc(sizeof(Tensor));
-    initialize_tensor(grad_output, batch_size, n_classes, grad);
+    initialize_tensor(grad_output, batch_size, out_features, grad);
     if (test_type == "backward") {
         grad_output->print();
     }
